461182add360ce49fc26b4ced0e6df2312e7e94e
Daws <castrogiraldosantiago@gmail.com>
2024-09-05 07:57:23 -0500

    migracion: paso 3

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 
+using DotNetEnv;
+using MSRosaAPI.Data;
+using Microsoft.EntityFrameworkCore;
+
 namespace MSRosaAPI;
 
 public class Program
@@ -8,6 +12,15 @@ public class Program
         var builder = WebApplication.CreateBuilder(args);
 
         // Add services to the container.
+        Env.Load();
+        var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+        var dbPort = Environment.GetEnvironmentVariable("DB_PORT");
+        var dbDatabaseName = Environment.GetEnvironmentVariable("DB_DATABASE");
+        var dbUser = Environment.GetEnvironmentVariable("DB_USERNAME");
+        var dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
+        var conectionDB = $"server={dbHost};port={dbPort};database={dbDatabaseName};uid={dbUser};password={dbPassword}";
+        builder.Services.AddDbContext<productosDbContext>(options => options.UseMySql(conectionDB, ServerVersion.Parse("8.0.20-mysql")));
+
 
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
