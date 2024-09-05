74b9f347c63c6196aae430dc7187f682b6ab5542
Daws <castrogiraldosantiago@gmail.com>
2024-09-05 08:03:59 -0500

    migraciones paso: 5

--- Models/Producto.cs
+++ Models/Producto.cs
@@ -1,12 +1,16 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MSRosaAPI.Models
 {
+    [Table("productos")]
     public class Producto
     {
+        [Key]
         public  int id{get;set;}
         public string NombreProducto{get;set;}
         public double PrecioUnitario{get;set;}
