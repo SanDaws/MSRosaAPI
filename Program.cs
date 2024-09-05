4060b3ed06ab8f155565e7fb119e6c70c85a0f86
Daws <castrogiraldosantiago@gmail.com>
2024-09-05 08:51:46 -0500

    scaffoldign paso fianl

--- Program.cs
+++ Program.cs
@@ -21,6 +21,17 @@ public class Program
         var conectionDB = $"server={dbHost};port={dbPort};database={dbDatabaseName};uid={dbUser};password={dbPassword}";
         builder.Services.AddDbContext<productosDbContext>(options => options.UseMySql(conectionDB, ServerVersion.Parse("8.0.20-mysql")));
 
+        var mpdbHost = Environment.GetEnvironmentVariable("DB_HOST");
+        var mpdbPort = Environment.GetEnvironmentVariable("DB_PORT");
+        var mpdbDatabaseName = Environment.GetEnvironmentVariable("DB_DATABASE");
+        var mpdbUser = Environment.GetEnvironmentVariable("DB_USERNAME");
+        var mpdbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
+
+        var mpconectionDB = $"server={mpdbHost};port={mpdbPort};database={mpdbDatabaseName};uid={mpdbUser};password={mpdbPassword}";
+        builder.Services.AddDbContext<DeudoresDbContext>(options => options.UseMySql(conectionDB, ServerVersion.Parse("8.0.20-mysql")));
+
+
+
 
         builder.Services.AddControllers();
         // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
